9adf512584957977b298e673823a728b018ab32f
Prashant Pandey <prashantpandey5959@gmail.com>
2023-06-16 10:54:14 +0530

    making-separate-for-parameter

--- src/App.js
+++ src/App.js
@@ -4,20 +4,31 @@ function App() {
   return (
     <>
       <h1>Props Demo</h1>
-      <ListDemo username="Prashant" email="Prashnant@gmail.com" />
-      <ListDemo username="Parul" email="parul@gmail.com" />
-      <ListDemo username="Aditya " email="Aditya@gmail.com" />
+      <ListDemo />
     </>
   );
 }
-    //by using destructring
-  function ListDemo({email, username}) { // ====>>>> this destructring is recommended
-  // let data="Hellow universe";
-  // let list=["delhi", "calcutta", "chennai"];
+//by applying this method we reduce the contents of main function
+function ListDemo() {
+  return (
+    <>
+      <Display />
+      <Display username="Prashant" email="Prashnant@gmail.com" />
+      <Display username="Parul" email="parul@gmail.com" />
+      <Display username="Aditya " email="Aditya@gmail.com" />
+    </>
+  );
+}
+
+//by using destructring
+function Display({ email, username }) {
+  // ====>>>> this destructring is recommended
 
   return (
     <>
-      <h1>Hello {username} {email}</h1>
+      <h1>
+        Hello {username} {email}
+      </h1>
       <h1>{username}</h1>
     </>
   );
