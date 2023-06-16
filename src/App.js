51dbc77812bdcda7f138557967d331b2d5c9eb58
Prashant Pandey <prashantpandey5959@gmail.com>
2023-06-16 12:18:58 +0530

    list-again-by-map

--- src/App.js
+++ src/App.js
@@ -4,34 +4,43 @@ function App() {
   return (
     <>
       <h1>Props Demo</h1>
-      <ListDemo />
+      <ListDemo/>
     </>
   );
 }
 //by applying this method we reduce the contents of main function
 function ListDemo() {
+  let string ="Hellow universe";
+  let list=["delhi","mumbai", "calcutta"];
+  // for(let i=0;i<10;i++){
+  //   list.push(string);
+  // }
+  console.log(list);
+
   return (
     <>
-      <Display />
-      <Display username="Prashant" email="Prashnant@gmail.com" />
-      <Display username="Parul" email="parul@gmail.com" />
-      <Display username="Aditya " email="Aditya@gmail.com" />
+     <h1>{string}</h1>
+     {/* for each is the consumer function ..so it doesn't return */}
+     {/**  {list.forEach((item)=>item)}; */}
+     
+     {/* map function  takes input and return output*/}
+     {list.map((item)=>item)};
     </>
   );
 }
 
 //by using destructring
-function Display({ email, username }) {
-  // ====>>>> this destructring is recommended
+// function Display({ email, username }) {
+//   // ====>>>> this destructring is recommended
 
-  return (
-    <>
-      <h1>
-        Hello {username} {email}
-      </h1>
-      <h1>{username}</h1>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <h1>
+//         Hello {username} {email}
+//       </h1>
+//       <h1>{username}</h1>
+//     </>
+//   );
+// }
 
 export default App;
